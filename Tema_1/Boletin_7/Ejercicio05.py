6b188e7f84a14d10742ee2a8c5bc57861138983a
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2022-10-17 10:33:24 +0200

    Terminados el ejercicio 05 y 07 (apuntes en el 07) del boletin 7

---
+++ Tema_1/Boletin_7/Ejercicio05.py
@@ -0,0 +1,11 @@
+numero = int(input(" Numero: "))
+
+historial = [numero]
+
+while numero != 1:
+    if numero%2==0:
+        numero = numero/2
+    else:
+        numero = numero*3+1
+    historial.append(numero)
+print(historial)    
---
+++ Tema_1/Boletin_7/Ejercicio07.py
@@ -0,0 +1,17 @@
+
+numero = int(input(" Numero: "))
+tmp = []
+for i in range (numero):
+    tmp.append(numero)
+    print(*tmp)     
+    
+    
+'''
+
+el poner el asterisco antes del tmp hace que la cadena se descomprima
+que he entendido que es algo asi como quitar las cosas que tiene al rededor
+
+
+por asi decirlo, el asterisco quita el formato a la cadena y la imprime sin corchetes ni comas
+
+'''
