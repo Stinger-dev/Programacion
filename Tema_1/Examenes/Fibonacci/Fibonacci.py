a8ff7e5ec418c7f98cbc70120fddb9814a92295d
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2022-10-29 14:14:27 +0200

    Añadido ejercicios fibonacci y comparacion entre recursivo y no recursivo

---
+++ Tema_1/Examenes/Fibonacci/Fibonacci.py
@@ -0,0 +1,16 @@
+tamagno = int(input('Cuantos numero: '))
+fibonacci = [1,1]
+
+while len(fibonacci)<tamagno:
+    fibonacci.append(fibonacci[-1]+fibonacci[-2])
+    
+
+#Esta parte de aqui es innecesaria pero queria que saliera la flechita
+cadena = ''
+separador = ' -> '
+for n in range(len(fibonacci)):
+    cadena += str(fibonacci[n]) + separador
+cadena = cadena[:(-len(separador))]
+
+print(cadena)
+
