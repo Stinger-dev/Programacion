e35415e3543884588332f171753c4bf43aa19d8c
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2022-11-16 18:49:37 +0100

    Terminado ej 3ç

--- Tema_2/Boletin_1/Ejercicio03.py
+++ Tema_2/Boletin_1/Ejercicio03.py
@@ -1,6 +1,6 @@
-dia = 15
-mes = 3
-agno = 2022
+dia = -1
+mes = -1
+agno = -1
 
 meses = ['enero','febrero','marzo','abril','mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre','diciembre']
 
@@ -11,14 +11,33 @@ def listaDiasAgno(agno):
         dias[1]=29
     return dias
 
-while ( not(0 < mes <= 12) and (mes > 0) ):
-    mes = input(' Mes no valido, pruebe de nuevo')
 
-if mes > 0:
-    while ( not(0 < mes <= 12) and (mes > 0) ):
-        mes = input(' Año no valido, pruebe de nuevo')   
+
+
+agno = int(input(' Indique el año: '))
+if agno >= 0:
+    mes = int(input(' Indique el mes: '))
+    while ( not(0 < mes <= 12) and (mes >= 0) ):
+        mes = int(input(' Mes no valido, pruebe de nuevo: '))
     
-    else:
-        while dia > listaDiasAgno(agno)[mes+1]:
-            dia = input(' Dia no valido, pruebe de nuevo')
+    if mes > 0:
+        dia = int(input(' Indique el dia: '))
+        while ((dia > listaDiasAgno(agno)[mes+1]) or (dia  == 0)):
+            dia = int(input(' Dia no valido, pruebe de nuevo: '))
             
+if dia > 0: #Como he inicializado el valor del dia a -1, la unica manera que la fecha sea correcta es que el dia sea mayor que uno, asi que asi puedo asegurarme que la fecha es correcta para poner la forma larga
+    print(f' La fecha en formato largo es {dia} de {meses[mes-1]} de {agno}')
+
+
+input('\n Saliendo')
+
+
+
+
+
+
+
+
+
+
+
