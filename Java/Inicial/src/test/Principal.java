df8282622723fbd7745e5db944411aa0f3c2a5ad
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2023-01-10 21:50:23 +0100

    Primeros pasos en Java

--- Java/Inicial/src/test/Principal.java
+++ Java/Inicial/src/test/Principal.java
@@ -1,9 +1,32 @@
-package test;
-
-public class Principal {
+	package test;
 	
-	public static void main(String[] args)
-	{
-		System.out.println("Hola mundo");
+	import java.util.Scanner;
+	
+	public class Principal {
+		
+		public static void main(String[] args)
+		
+		{	
+			
+			
+			Scanner sc = new Scanner (System.in);
+			
+			System.out.print("Introduce algo: ");
+			String cadena =  sc.nextLine();
+			
+			System.out.println("hola a todos"+ cadena);
+			
+			
+			cadena =  sc.nextLine();
+	
+			System.out.println("hola a todos"+ cadena);	
+			
+			
+			for (int i=0;i<10;i++)
+			{
+				System.out.println(i);
+			}
+			
+		}
 	}
-}
+		
