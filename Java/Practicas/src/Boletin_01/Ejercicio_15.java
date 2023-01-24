2d999b5a45bf797493d433e0ab02752c77310752
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2023-01-24 12:10:08 +0100

    Avances boletin t01b02 java

--- Java/Practicas/src/Boletin_01/Ejercicio_15.java
+++ Java/Practicas/src/Boletin_01/Ejercicio_15.java
@@ -6,17 +6,13 @@ public class Ejercicio_15 {
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
-		String resultado;
+		String resultado = "0";
 
 		float k = 0;
 
 		do {
-
+			k += Float.valueOf(resultado);
 			resultado = String.valueOf(teclado.nextLine());
-
-			if (!resultado.equalsIgnoreCase("fin"))
-				k += Float.valueOf(resultado);
-
 		} while (!resultado.equalsIgnoreCase("fin"));
 
 		System.out.println(k);
--- Java/Practicas/src/Boletin_02/Boletin_completo.java
+++ Java/Practicas/src/Boletin_02/Boletin_completo.java
@@ -4,7 +4,8 @@ public class Boletin_completo {
 
 	public static void main(String[] args) {
 
-		System.out.println(invertirCadena("Hola mundo"));
+		System.out.println(toBinary(111));
+
 	}
 
 	public static int numeroSolucionesEcuacionSegundoGrad(float a, float b, float c) {
@@ -29,7 +30,6 @@ public class Boletin_completo {
 	}
 
 	public static boolean esFuerte(String contra) {
-		boolean segura = false;
 		// (mas de 8, numeros, minusculas, mayusculas, simbolos)
 		int[] check = { 0, 0, 0, 0, 0 };
 
@@ -64,8 +64,11 @@ public class Boletin_completo {
 
 		String resultado = "";
 
+		int tmp = (palabra.length() % 2 == 0 ? 0 : 1); // Con esto le resto uno al tamaño en el caso que uno de los
+														// caracteres este suelto
+
 		int k = 1;
-		for (int n = 0; n < palabra.length(); n++) {
+		for (int n = 0; n < palabra.length() - tmp; n++) {
 
 			resultado += palabra.charAt(k);
 
@@ -75,7 +78,57 @@ public class Boletin_completo {
 				k -= 1;
 			}
 		}
+		if (palabra.length() % 2 != 0) // si hay un caracter suelto esto lo añade al final
+			resultado += palabra.charAt(palabra.length() - 1);
+
 		return resultado;
 	}
-	
+
+	public static boolean esMultiplo(int a, int b) {
+		return (a % b == 0 ? true : false);
+	}
+
+	public static int horaMayor(int[] h1, int[] h2) {
+
+		int resultado = -1000;
+
+		if ((h1[0] > 0 && h1[0] < 24) && (h1[1] > 0 && h1[1] < 60) && (h1[2] > 0 && h1[2] < 60)) {
+			if ((h2[0] > 0 && h2[0] < 24) && (h2[1] > 0 && h2[1] < 60) && (h2[2] > 0 && h2[2] < 60)) {
+
+				if (h1[0] > h2[0]) {
+					resultado = 1;
+				} else if (h1[0] < h2[0]) {
+					resultado = 2;
+				} else {
+					if (h1[1] > h2[1]) {
+						resultado = 1;
+					} else if (h1[1] < h2[1]) {
+						resultado = 2;
+					} else {
+						if (h1[2] > h2[2]) {
+							resultado = 1;
+						} else if (h1[2] < h2[2]) {
+							resultado = 2;
+						} else {
+							resultado = 0;
+						}
+					}
+				}
+			}
+		}
+		return resultado;
+	}
+
+	public static String toBinary(int a) {
+
+		String resultado1 = "", resultadoF = "";
+		while (a > 0) {
+			resultado1 += (a % 2);
+			a = ((a - (a % 2)) / 2);
+		}
+		for (int i = resultado1.length() - 1; i >= 0; i--) {
+			resultadoF += resultado1.charAt(i);
+		}
+		return resultadoF;
+	}
 }
